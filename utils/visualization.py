2a37b7a8142b5d7b4f089c3eefb0f831989598bc
gitkeniwo <kennethwwwww@gmail.com>
2024-06-15 23:27:20 +0200

    update visualization

--- utils/visualization.py
+++ utils/visualization.py
@@ -80,6 +80,104 @@ def plot_trace(df: pd.DataFrame, mode: str="position-only",
         fig.update_layout(height=900, width=800, title_text="Trace")
         
     fig.show()
+ 
+ 
+ 
+ 
+def range_finder(spoofed_data, benign_data): 
+    "decide the range of x and y axis for the plot"
+    x_range_s, y_range_s = (spoofed_data.GPS_lat.min(), 
+            spoofed_data.GPS_lat.max()), \
+            (spoofed_data.GPS_long.min(), 
+            spoofed_data.GPS_long.max())
+            
+    x_range_b, y_range_b = (benign_data.GPS_lat.min(), 
+            benign_data.GPS_lat.max()), \
+            (benign_data.GPS_long.min(), 
+            benign_data.GPS_long.max())
+            
+    return (min(x_range_s[0], x_range_b[0]), 
+            max(x_range_s[1], x_range_b[1])), \
+            (min(y_range_s[0], y_range_b[0]), 
+            max(y_range_s[1], y_range_b[1]))
+ 
+ 
+def plot_spoofed_and_benign(spoofed_list, benign_list,
+                            marker_size=2):
+    
+    len_trace = len(spoofed_list)
+     
+    fig = make_subplots(rows=len_trace, cols=2)
+    
+    for i, (s, b) in enumerate([(i,j) for i,j in zip(spoofed_list, benign_list)]):
+        
+        y_range, x_range = range_finder(s, b)
+        
+        plot_spoofed_and_benign_trace(fig, row=i+1, spoofed=s, benign=b,
+                                      x_range=x_range, y_range=y_range)
+        
+
+    fig.update_traces(marker=dict(size=marker_size))
+    
+    fig.update_layout(height=200 * len_trace, width=600, title_text="Trace")
+    
+    # disable legend
+    fig.update_layout(showlegend=False)
+    
+    # save the figure to png
+    fig.write_image("../outputs/img/traces.png")
+    fig.show()
+    
+         
+ 
+ 
+def plot_spoofed_and_benign_trace(fig: go.Figure, row: int, spoofed: pd.DataFrame, benign: pd.DataFrame,
+                            marker_size=2, x_range=(51.43, 51.56), y_range=(25.28, 25.39),
+                            colored: bool=True, color_col: str="spoofed",
+                            spoofed_color: str="red", genuine_color: str="green"):
+    
+    if colored:
+        colors_spoofed = spoofed[color_col].apply(lambda x: spoofed_color if x==1 else genuine_color)
+        colors_benign = benign[color_col].apply(lambda x: spoofed_color if x==1 else genuine_color)
+        
+        fig.add_trace(
+            go.Scatter(x=spoofed['GPS_long'], y=spoofed['GPS_lat'], 
+                        mode='markers',
+                        marker=dict(color=colors_spoofed)),
+            row=row, col=1, 
+        )
+        
+        fig.add_trace(
+            go.Scatter(x=benign['GPS_long'], y=benign['GPS_lat'], 
+                        mode='markers',
+                        marker=dict(color=colors_benign)),
+            row=row, col=2, 
+        )
+    
+    else:
+        fig.add_trace(
+            go.Scatter(x=spoofed['GPS_long'], y=spoofed['GPS_lat']),
+            row=row, col=1,
+        )
+        
+        fig.add_trace(
+            go.Scatter(x=benign['GPS_long'], y=benign['GPS_lat']),
+            row=row, col=2,
+        )
+
+    fig.update_xaxes(range=[x_range[0], x_range[1]], row=row, col=1)
+    fig.update_yaxes(range=[y_range[0], y_range[1]], row=row, col=1)
+    fig.update_xaxes(range=[x_range[0], x_range[1]], row=row, col=2)
+    fig.update_yaxes(range=[y_range[0], y_range[1]], row=row, col=2)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
     
     
     
@@ -123,6 +221,10 @@ def plot_pca(pca_dfs: pd.DataFrame, n_components: int=2):
     else:
         raise ValueError("n_components must be either 2 or 3")
     
+    
+
+
+
     
 def plot_cf_matrix(cf_matrix: np.ndarray):
     """
