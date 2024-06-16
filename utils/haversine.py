c39738df89928745ac116995deee85e371b76727
gitkeniwo <kennethwwwww@gmail.com>
2024-06-16 19:25:09 +0200

    update haversine dependence

--- utils/haversine.py
+++ utils/haversine.py
@@ -24,3 +24,30 @@ def haversine(lat, lon):
     distance = R * c
 
     return distance
+
+def haversine_distance(point_1, point_2):
+    # Radius of the Earth in kilometers
+    R = 6371
+
+    # Convert degrees to radians
+
+
+    # Differences in coordinates
+    dlat = point_1[0] - point_2[0]
+    dlon = point_2[1] - point_1[1]
+
+    # Haversine formula
+    a = math.sin(dlat / 2)**2 + math.cos(point_1[0]) * math.cos(point_2[0]) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    # Distance in kilometers
+    distance = R * c
+
+    return distance
+
+def haversine_component_distance(p1, p2):
+    
+    p3 = (p1[0], p2[1])
+    
+    return haversine_distance(p3, p2), haversine_distance(p1, p3)
+
